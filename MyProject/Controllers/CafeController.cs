fdcd0c1463f0c93cd1890595340777adc657f02c
Yana <77960186+yanakrupets@users.noreply.github.com>
2021-07-04 13:04:09 +0300

    links

---
+++ MyProject/Controllers/CafeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Controllers
+{
+    public class CafeController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
