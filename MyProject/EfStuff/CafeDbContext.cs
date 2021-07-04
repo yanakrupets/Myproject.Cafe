770062ae58cc7a8bf345fab6610f84975b1da02f
Yana <77960186+yanakrupets@users.noreply.github.com>
2021-07-04 12:17:24 +0300

    main page

---
+++ MyProject/EfStuff/CafeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.EfStuff
+{
+    public class CafeDbContext : DbContext
+    {
+        public CafeDbContext(DbContextOptions options) : base(options) { }
+    }
+}
